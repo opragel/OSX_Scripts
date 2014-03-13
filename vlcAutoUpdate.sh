962b2a46b87ca9e4390bf32aa84feab89cb482fa
Owen Pragel <owen@74bit.com>
2014-03-13 15:49:50 -0700

    Updated VLC auto download script
    Now looks for last version number in directory instead of relying on
    'last' directory which isn't always updated. This script should be
    cleaned up, especially in regard to the variables.

--- vlcAutoUpdate.sh
+++ vlcAutoUpdate.sh
@@ -1,38 +1,36 @@
-  #!/bin/bash
-  # Downloads the most recent version of VLC and installs it
-  # Places installers into /tmp which is cleared on reboot
-  # Created by Owen Pragel on 11/8/2013
+#!/bin/bash
+# Downloads the most recent version of VLC and installs it
+# Places installers into /tmp which is cleared on reboot
+# Created by Owen Pragel on 11/8/2013.
+# Updated 3/14/14
 
-  appName='VLC'
+appName='VLC'
 
-  echo $appName" auto-update and install"
+echo $appName" auto-update and install"
 
-  downloadDirectory='http://get.videolan.org:81/vlc/last/macosx/'
-  localDirectory='/tmp/'
+downloadDirectory='http://get.videolan.org:81/vlc/'
+localDirectory='/tmp/'
 
-  # Regex to match latest version string
-  versionRegex='vlc-......dmg'
+# Regex to match latest version string
+versionRegex='vlc-......dmg'
 
-  # Store the file name of the most curent version to be used at the end of directory url
-  #
-  currentVersionFile=$(curl -s $downloadDirectory | grep -m1 -o $versionRegex | head -1)
-  currentVersion=$(echo $currentVersionFile | grep -o -E '[0-9]+\.[0-9]+\.[0-9]')
-  echo "Current Version: "$currentVersionFile
-  echo "Shortened Version: "$currentVersion
+# Store the file name of the most curent version to be used at the end of directory url
 
-  # Concatenates download directory and current version filename (ex. http://web.com/ + download.zip)
-  fileURL=$downloadDirectory$currentVersionFile
-  echo "Downloading: $fileURL"
-  echo -e "To: $localDirectory$currentVersionFile\n"
-  curl $fileURL -L -o $localDirectory$currentVersionFile -w '%{url_effective}'
+currentVersion=$(curl $downloadDirectory | grep -o \"[0-9].[0-9].[0-9] | tail -c 6)
 
-  volumePath=$(hdiutil attach $localDirectory$currentVersionFile | grep -o -E -m1 '/Volumes/vlc-.....')
+# Concatenates download directory and current version filename (ex. http://web.com/ + download.zip)
+fileURL=$downloadDirectory$currentVersion"/macosx/vlc-"$currentVersion".dmg"
+echo "Downloading: $fileURL"
+echo -e "To: $localDirectory"vlc-"$currentVersion\n"
+curl $fileURL -L -o $localDirectory"vlc-"$currentVersion -w '%{url_effective}'
 
-  appPath="/VLC.app"
+volumePath=$(hdiutil attach $localDirectory"vlc-"$currentVersion | grep -o -E -m1 '/Volumes/vlc-.....')
 
-  echo "Copying "$volumePath$appPath" to "/Applications$appPath
-  cp -R $volumePath$appPath /Applications$appPath
+appPath="/VLC.app"
 
-  hdiutil detach $volumePath
+echo -n "Copying "$volumePath$appPath" to "/Applications$appPath
+cp -R $volumePath$appPath /Applications$appPath
 
-  exit 0
+hdiutil detach $volumePath
+
+exit 0
