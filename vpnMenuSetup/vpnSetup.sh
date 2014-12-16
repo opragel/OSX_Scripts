c172c9e7fa5cd78315c70b53ebb5b1a8be22fb3c
owen-74bit <owen@74bit.com>
2014-12-16 15:21:03 -0800

    Added sus back in for permissions
    Last changes broke file permissions.

--- vpnMenuSetup/vpnSetup.sh
+++ vpnMenuSetup/vpnSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script adds the VPN item to the menu bar and enables the show time 
-# connected and show status while connecting options. Tested on OS X 10.9 & 10.10
+# connected and show status while connecting options. Tested on OS X 10.9.
 
 lastUser=$(defaults read /Library/Preferences/com.apple.loginwindow lastUserName)
 vpnMenuItem=$(defaults read "/Users/$lastUser/Library/Preferences/com.apple.systemuiserver" menuExtras | grep -o VPN)
@@ -8,11 +8,11 @@ vpnMenuItem=$(defaults read "/Users/$lastUser/Library/Preferences/com.apple.syst
 if [ "$vpnMenuItem" ]; then
 	echo "VPN menu item is already on the status bar!"
 else
-	defaults write "/Users/$lastUser/Library/Preferences/com.apple.systemuiserver" menuExtras -array-add "/System/Library/CoreServices/Menu Extras/VPN.menu"
+	su "$lastUser" defaults write "/Users/$lastUser/Library/Preferences/com.apple.systemuiserver" menuExtras -array-add "/System/Library/CoreServices/Menu Extras/VPN.menu"
 fi
 
-defaults write "/Users/$lastUser/Library/Preferences/com.apple.networkConnect" VPNShowStatus -bool true
-defaults write "/Users/$lastUser/Library/Preferences/com.apple.networkConnect" VPNShowTime -bool true
+su "$lastUser" defaults write "/Users/$lastUser/Library/Preferences/com.apple.networkConnect" VPNShowStatus -bool true
+su "$lastUser" defaults write "/Users/$lastUser/Library/Preferences/com.apple.networkConnect" VPNShowTime -bool true
 killall SystemUIServer
 
 exit 0
