89d210e11d04303449b0d34a71ba8691571dfe0f
Owen Pragel <owen.pragel@74bit.com>
2015-04-25 00:24:07 -0700

    Removed whereami script

--- authorizeWhereami.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Runs whereami as the current user so they can allow/deny location services
-# Created by Owen Pragel on September 19th
-lastUser=`defaults read /Library/Preferences/com.apple.loginwindow lastUserName`
-sudo -u $lastUser /usr/bin/./whereami
