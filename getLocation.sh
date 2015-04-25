8dc7733e3bf2280435c96d447a71dfe3e278dd0b
Owen Pragel <owen.pragel@74bit.com>
2015-04-25 00:31:07 -0700

    Major cleanup, removed a number of old scripts. Placed each script into its own folder.

--- getLocation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Created by Owen Pragel on September 7th, 2013. Credit to
-echo "<result>"
-/usr/bin/./whereami | grep -A 1 'Latitude\|services\|Wi-Fi'
-echo "</result>"
--- pipeSublimeToConsole.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-osascript -e '
-    on run parameters
-        tell application "Terminal"
-            activate
-            do script with command "python " & parameters
-        end tell
-    end run
-' $@
